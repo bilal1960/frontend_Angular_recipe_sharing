9683f548e716e60e64c97e4f73208672a989610e
bilal1960 <bilal45001@hotmail.com>
2024-04-08 04:15:38 +0200

    login and signup finish

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -2,32 +2,47 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import {FormGroup, FormsModule, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule, MatRadioModule],
+  imports: [CommonModule, MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule, MatRadioModule,ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
 export class AuthComponent {
 
-  isRegister=false
+  isRegister=false;
 
-  recipeItem:any={
-    title:"",
-    description:"",
-    foodType:"",
-    image:""
+  registertionForm=new FormGroup({
+    FullName: new FormControl("",[Validators.required]),
+    email: new FormControl("",[Validators.required, Validators.email]),
+    password: new FormControl("",[Validators.required, Validators.minLength(6)
+
+    ]),
+
+  });
+
+  loginForm=new FormGroup({
+    email: new FormControl("",[Validators.required, Validators.email]),
+
+  });
+
+  handleRegister(){
+    console.log("register",this.registertionForm.value)
+
   }
 
-  onSubmit(){
-
-    console.log("values",this.recipeItem)
-
+  handleLogin(){
+    console.log("login",this.loginForm.value)
   }
+
+  togglePanel(){
+    this.isRegister=!this.isRegister
+  }
+
 
 }
